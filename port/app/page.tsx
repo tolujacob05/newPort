8cda17d5a5578f7be6ed4aabf536aeaf4dc97bbd
tolujacob05 <tolujacob05@gmail.com>
2024-06-10 12:57:43 +0100

    first commit

---
+++ port/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Navbar } from "./navbar/page";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navbar />
+    </main>
+  );
+}
